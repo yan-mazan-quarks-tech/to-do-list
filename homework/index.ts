a9668f783cea677767e937d4a951f8c847e44b8d
Yan Mazan <yan.mazan@quarks.tech>
2024-05-28 10:13:00 +0300

    use different port

--- homework/index.ts
+++ homework/index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
-const port = 3001;
+const port = 3002;
 const app: Express = express();
 
 app.get("/test", (req: Request, resp: Response) => {
